aca692cdfe92e6e75d03548b977ada233f8b3418
noadanon220 <35223664+noadanon220@users.noreply.github.com>
2025-06-08 00:07:55 +0300

    Reminder - update relevent date in item_reminder

--- PawsitiveLife/app/src/main/java/com/example/pawsitivelife/adapter/ReminderViewHolder.kt
+++ PawsitiveLife/app/src/main/java/com/example/pawsitivelife/adapter/ReminderViewHolder.kt
@@ -18,16 +18,17 @@ class ReminderViewHolder(
 
     private val title: TextView = itemView.findViewById(R.id.reminder_title)
     private val time: TextView = itemView.findViewById(R.id.reminder_time)
+    private val date: TextView = itemView.findViewById(R.id.reminder_date)
     private val image: ImageView = itemView.findViewById(R.id.reminder_image)
-
-    private val formatter = DateTimeFormatter.ofPattern("HH:mm")
-
     private val notes: TextView = itemView.findViewById(R.id.reminder_notes)
 
+    private val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+    private val dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
 
     fun bind(reminder: Reminder) {
         title.text = reminder.title
-        time.text = reminder.date.format(formatter)
+        time.text = reminder.date.format(timeFormatter)
+        date.text = reminder.date.format(dateFormatter)
 
         val imagePath = reminder.imagePath
         val file = if (!imagePath.isNullOrBlank()) File(imagePath) else null
@@ -46,6 +47,4 @@ class ReminderViewHolder(
             onReminderClick?.invoke(reminder)
         }
     }
-
-
 }
